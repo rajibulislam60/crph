fe41830bd5a042d77217f771ac93a314a994489d
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-11-25 16:19:39 +0600

    react and tailwind setup and props bubbling done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Bubbling from "./propBubbling/bubbling";
+
+const App = () => {
+  return (
+    <div>
+      <Bubbling />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/propBubbling/bubbling.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const bubbling = () => {
+    let handlePerant = (event)=>{
+        console.log("event of perant: ", event)
+    }
+    let handleSubmit = (event)=>{
+        event.stopPropagation();
+        console.log("event of child: ", event)
+    }
+  return (
+    <div onClick={handlePerant} className='bg-slate-200 py-10 px-10'>
+        <button onClick={handleSubmit} className='border bg-teal-100 rounded-md px-5 py-3'>Submit</button>
+    </div>
+  )
+}
+
+export default bubbling
