190a9cc4d07b519563863f981d219cb261a8f0cb
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-11-28 11:57:02 +0600

    update bubbling

--- src/propBubbling/bubbling.jsx
+++ src/propBubbling/bubbling.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const bubbling = () => {
-  let handlePerant = (event) => {
-    console.log("event of perant: ", event);
+  let [takeConsole, setTakeConsole] = useState([])
+  let handleParent = (event) => {
+    setTakeConsole((prev) => [...prev, `Event of parent: ${event}`]);
   };
   let handleSubmit = (event) => {
     event.stopPropagation();
-    console.log("event of child: ", event);
+    setTakeConsole((prev) => [...prev, `Event of child: ${event}`]);
   };
   return (
-    <div onClick={handlePerant} className="bg-slate-200 py-10 px-10">
+    <>
+    <div onClick={handleParent} className="bg-slate-200 py-10 px-10">
+      <p>Just click and try</p>
       <button
         onClick={handleSubmit}
         className="border bg-teal-100 rounded-md px-5 py-3"
       >
         Submit
       </button>
+      
     </div>
+    {takeConsole.map((item)=>(
+        <p>{item}</p>
+      ))}
+    </>
+    
   );
 };
 
